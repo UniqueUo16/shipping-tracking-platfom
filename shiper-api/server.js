c0411778bf43cee4a36a542e14cbf55ad3279071
Unique Uo <Uniqueuo010@gmail.com>
2025-10-27 00:00:59 +0100

    Final Mvp Edit

--- shiper-api/server.js
+++ shiper-api/server.js
@@ -266,4 +266,5 @@ app.delete("/api/bookings/:id", (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("🌐 Server running at http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🌐 Server running on port ${PORT}`));
